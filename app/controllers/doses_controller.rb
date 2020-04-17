d3c1b8bfd4e7a8b74abe3c4c50fcdaa27da7ee49
Juliana Ariffin <juliana@fastmail.com>
2020-04-17 12:52:46 +0200

    actions done

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,23 @@
+class DosesController < ApplicationController
+  def new
+    @cocktail = Cocktail.new(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new
+    @cocktail = Cocktail.new(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+end
